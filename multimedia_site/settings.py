aa4afe23acd53e490a40fb63fad8944354f5a03b
Waptech <abdulfataiyakub07@gmail.com>
2023-12-11 07:00:25 +0100

    ready to deploy

--- multimedia_site/settings.py
+++ multimedia_site/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, 'templates')],
+            os.path.join(BASE_DIR, 'multimedia/templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,11 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
